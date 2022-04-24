5e0c364fe94f66e65fdb54abae3b368d9b4c854b
sudosubin <sudosubin@gmail.com>
2022-04-24 17:44:26 +0900

    feat: Use launchd for darwin

--- module/darwin.nix
+++ module/darwin.nix
@@ -1,8 +1,26 @@
 { config, pkgs, lib, ... }:
 { script }:
 
+let
+  scriptBin = pkgs.writeShellScriptBin "home-manager-secrets-script" ''
+    set -euo pipefail
+    DRY_RUN_CMD=
+    VERBOSE_ARG=
+    ${script}
+  '';
+
+in
 {
-  home.activation = {
-    homeManagerSecrets = lib.hm.dag.entryAfter [ "writeBoundary" ] script;
+  launchd.agents.home-manager-secrets = {
+    enable = true;
+    config = {
+      ProgramArguments = [ "${scriptBin}/bin/home-manager-secrets-script" ];
+      RunAtLoad = true;
+      EnvironmentVariables = {
+        PATH = "${lib.makeBinPath [ pkgs.coreutils ]}";
+      };
+      StandardOutPath = "${config.xdg.cacheHome}/home-manager-secrets.log";
+      StandardErrorPath = "${config.xdg.cacheHome}/home-manager-secrets.log";
+    };
   };
 }
